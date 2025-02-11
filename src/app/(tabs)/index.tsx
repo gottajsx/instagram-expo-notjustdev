6b0fc278023c1af47afc22fe3f49af3bb26e9618
Sebastien Schneider <sebastien.schneider.external@spherea.com>
2025-02-11 13:40:58 +0100

    Building the New Post Screen frontend

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -8,7 +8,12 @@ export default function FeedScreen() {
         <FlatList 
             data={posts}
             renderItem={({ item }) => <PostListItem post={item} />}
-            contentContainerStyle={{ gap: 10}}
+            contentContainerStyle={{ 
+                gap: 10, 
+                maxWidth: 512, 
+                width: '100%',
+                alignSelf: 'center'
+            }}
             showsVerticalScrollIndicator={false}
         />
     );
