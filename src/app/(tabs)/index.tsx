d25e636d6a83c93ee498dc94196ab860c24dc1ea
Sebastien Schneider <sebastien.schneider.external@spherea.com>
2025-02-11 11:24:26 +0100

    Rendering only one first post

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import posts from '~/assets/data/posts.json';
+import { Ionicons, Feather, AntDesign} from "@expo/vector-icons";
+
+const post1 = posts[0];
 
 export default function FeedScreen() {
+    console.log(post1);
+
     return (
-        <View className='bg-slate-600 flex-1 items-center justify-center'>
-            <Text className='text-5xl font-bold text-blue-500'>Feed</Text>
+        <View className="bg-white">
+            {/* Header */}
+            <View className='p-3 flex-row items-center gap-2'>
+                <Image 
+                    source={{ uri: post1.user.image_url }} 
+                    className='w-12 aspect-square rounded-full'
+                />
+                <Text className="font-semibold">{post1.user.username}</Text>
+            </View>
+            <Image 
+                source={{ uri: post1.image_url }} 
+                className='w-full aspect-[4/3]'
+            />
+            {/* Icons */}
+            <View className='flex-row gap-3 p-3'>
+                <AntDesign name="hearto" size={20} />
+                <Ionicons name="chatbubble-outline" size={20} />
+                <Feather name="send" size={20} />
+
+                <Feather name="bookmark" size={20} className="ml-auto" />
+            </View>
         </View>
     )
     
