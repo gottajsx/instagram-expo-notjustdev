16e203e98273819c70d55b50c5d03aeb4b01281b
Sebastien Schneider <sebastien.schneider.external@spherea.com>
2025-02-11 11:55:42 +0100

    Ending the User Interface Feed Screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,15 @@
-import { Text, View, Image } from 'react-native';
+import { FlatList } from 'react-native';
 import posts from '~/assets/data/posts.json';
-import { Ionicons, Feather, AntDesign} from "@expo/vector-icons";
-
-const post1 = posts[0];
-
+import PostListItem from '~/src/components/PostListItem';
+   
 export default function FeedScreen() {
-    console.log(post1);
 
-    return (
-        <View className="bg-white">
-            {/* Header */}
-            <View className='p-3 flex-row items-center gap-2'>
-                <Image 
-                    source={{ uri: post1.user.image_url }} 
-                    className='w-12 aspect-square rounded-full'
-                />
-                <Text className="font-semibold">{post1.user.username}</Text>
-            </View>
-            <Image 
-                source={{ uri: post1.image_url }} 
-                className='w-full aspect-[4/3]'
-            />
-            {/* Icons */}
-            <View className='flex-row gap-3 p-3'>
-                <AntDesign name="hearto" size={20} />
-                <Ionicons name="chatbubble-outline" size={20} />
-                <Feather name="send" size={20} />
-
-                <Feather name="bookmark" size={20} className="ml-auto" />
-            </View>
-        </View>
-    )
-    
+    return(
+        <FlatList 
+            data={posts}
+            renderItem={({ item }) => <PostListItem post={item} />}
+            contentContainerStyle={{ gap: 10}}
+            showsVerticalScrollIndicator={false}
+        />
+    );
 }
