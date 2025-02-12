adaebd18759c68c77db27db1fdbc610e961becac
Sebastien Schneider <sebastien.schneider.external@spherea.com>
2025-02-12 10:44:09 +0100

    Setting up cloudinary images

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,21 +1,39 @@
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, useWindowDimensions } from 'react-native';
 import { Ionicons, Feather, AntDesign} from "@expo/vector-icons";
 
+import { AdvancedImage } from 'cloudinary-react-native';
+import { Cloudinary } from "@cloudinary/url-gen"; 
+// Import required actions and qualifiers.
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { cld } from "~/src/lib/cloudinary";
+
 export default function PostListItem({ post }) {
+    const { width } = useWindowDimensions();
+    const roundedWidth = Math.round(width)
+
+    const image = cld.image(post.image);
+    image.resize(thumbnail().width(roundedWidth).height(roundedWidth));
+
+    const avatar = cld.image(post.user.avatar_url);
+    avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())));
+    
     return (
         <View className="bg-white">
             {/* Header */}
             <View className='p-3 flex-row items-center gap-2'>
-                <Image 
-                    source={{ uri: post.user.image_url }} 
+                <AdvancedImage
+                    cldImg={avatar} 
                     className='w-12 aspect-square rounded-full'
                 />
                 <Text className="font-semibold">{post.user.username}</Text>
             </View>
-            <Image 
-                source={{ uri: post.image_url }} 
-                className='w-full aspect-[4/3]'
-            />
+
+            {/* Content */}
+            <AdvancedImage cldImg={image} className='w-full aspect-[4/3]' />
+
             {/* Icons */}
             <View className='flex-row gap-3 p-3'>
                 <AntDesign name="hearto" size={20} />
