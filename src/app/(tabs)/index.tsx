e32f0bfeb1c1b8dfa15094eb4e7482490615082c
Sebastien Schneider <sebastien.schneider.external@spherea.com>
2025-02-13 11:34:52 +0100

    Loading and pulling images on database

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,37 @@
-import { FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
 import posts from '~/assets/data/posts.json';
 import PostListItem from '~/src/components/PostListItem';
+import { supabase } from '~/src/lib/supabase';
+import { useAuth } from '~/src/providers/AuthProvider';
    
 export default function FeedScreen() {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const { user } = useAuth();
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+    const fetchPosts = async () => {
+        setLoading(true);
+        let { data, error } = await supabase 
+            .from('posts') 
+            .select('*, user:profiles(*)')
+            .order('created_at', { ascending: false });
+        if (error) {
+            Alert.alert("Something went wrong")
+        }
+        else {
+            setPosts(data);
+            setLoading(false);
+        }
+    }
+
+
 
     return(
         <FlatList 
@@ -15,6 +44,8 @@ export default function FeedScreen() {
                 alignSelf: 'center'
             }}
             showsVerticalScrollIndicator={false}
+            onRefresh={fetchPosts}
+            refreshing={loading}
         />
     );
 }
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -3,10 +3,15 @@ import { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import Button from '~/src/components/Button';
 import { uploadImage } from '~/src/lib/cloudinary';
+import { supabase } from '~/src/lib/supabase';
+import { useAuth } from '~/src/providers/AuthProvider';
+import { router } from 'expo-router';
 
 export default function CreatePost() {
     const [caption, setCaption] = useState('');
     const [image, setImage] = useState<string | null>(null);
+
+    const { session } = useAuth();
 
     useEffect(() => {
         if (!image) {
@@ -33,8 +38,22 @@ export default function CreatePost() {
             return;
         }
         const response = await uploadImage(image);
+
         console.log('image id:', response?.public_id);
+        
         // save the post in database
+        const { data, error } = await supabase
+            .from('posts')
+            .insert([
+                { 
+                    caption, 
+                    image: response?.public_id, 
+                    user_id: session?.user.id 
+                }
+            ])
+            .select()
+        
+        router.push('/(tabs)')
     };
 
     return (
